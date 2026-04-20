ee6acd35d69ddca262b5cae9d51467430ede2c69
Victor <tubebigbig@gmail.com>
2026-04-20 23:50:41 +0800

    refactor(docs): drop PwaUpdateToast from VitePress theme

--- apps/docs/.vitepress/theme/index.ts
+++ apps/docs/.vitepress/theme/index.ts
@@ -1,7 +1,6 @@
 import { h } from 'vue';
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
-import PwaUpdateToast from './PwaUpdateToast.vue';
 import HeroInstall from './HeroInstall.vue';
 import './style.css';
 
@@ -9,7 +8,6 @@ export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
-      'layout-bottom': () => h(PwaUpdateToast),
       'home-hero-image': () => h(HeroInstall),
     });
   },
